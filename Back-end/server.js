3cdd9d41cb8dab1e5960d1a76f304a666b2fdd1d
Vit TheVicious <vitor.rehem.sa@gmail.com>
2024-11-25 19:57:45 +0000

    finalização do login

--- Back-end/server.js
+++ Back-end/server.js
@@ -59,6 +59,50 @@ app.post('/api/register', (req, response) =>{
     })
 }); 
 
+app.post("/login", (request, response)  => {
+    const user = request.body.user
+
+   const searchCommand = `
+    SELECT * FROM Users
+    WHERE email = ?
+   `
+
+   db.query(searchCommand, [user.email], (error, data) => {
+    if(error) {
+        console.log(error)
+        return
+    }
+
+    if(data.length === 0) {
+        response.json({message:"Não existe nenhum usuário cadastrado com esse e-mail!"})
+        return
+    }
+
+    if(user.password === data[0].password) {
+        const email = user.email
+        const id = data[0].id
+        const token = jwt.sign({ id, email }, SECRET_KEY, {expiresIn: "1h"})
+        response.json({token, ok: true})
+        return;
+    }
+
+    response.json({ message: "Credêdncias invalidas! Tente novamente"})
+   })
+})
+
+app.get("/verify", (request, response) => {
+    const token = request.headers.authorization
+
+    jwt.verify(token, SECRET_KEY,  (error) => {
+        if (error) {
+            response.json({ message: "Token inválido! Efetue o login novamente."})
+            return
+        }
+
+        response.json({ ok: true })
+    })
+})
+
 app.post('/api/score', (req, response) => {
     
 })
